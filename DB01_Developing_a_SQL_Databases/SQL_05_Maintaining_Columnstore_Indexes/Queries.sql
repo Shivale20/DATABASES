5b1340216c66d0c798faf1823a8d257625140ac0
Manish Shivale <111711726+Shivale20@users.noreply.github.com>
2024-03-22 15:43:26 -0400

    Add SQL_05 in DB01

--- DB01_Developing_a_SQL_Databases/SQL_05_Maintaining_Columnstore_Indexes/Queries.sql
+++ DB01_Developing_a_SQL_Databases/SQL_05_Maintaining_Columnstore_Indexes/Queries.sql
@@ -3,6 +3,252 @@
   Demonstrate how to reorganize a columnstore index
 **************************************************************/
 
+-- check the fragmentation of the entire indexes in the dimSales_2011 table
+
+/***
+LOGIC:
+- I need fragmentation % for each index in db
+- so index id, index name and frag%
+- frag% will come from ?
+- index info will come from ?
+- join both on ?
+
+- sys.dm_db_index_physical_stats() function returns information about the physical characteristics of indexes.
+- useful for analyzing the 
+
+***/
+
+SELECT *
+FROM sys.indexes;
+
+-- return the object id for dimension table.
+select *
+from sys.objects
+where name = 'dimSales_2011';
+
+-- return info for index fragmentation stats for dimension table
+select *
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  )
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011')
+
+-- return index name and index id for dimension table
+SELECT index_id, name as index_name
+FROM sys.indexes
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011')
+
+ -- return index id, index name and avg fragmentation in percent from both tables
+select
+  a.index_id,
+  b.name as index_name,
+  a.avg_fragmentation_in_percent as degree
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) as a
+  JOIN sys.indexes as b
+  ON a.index_id = b.index_id
+WHERE b.object_id = (
+  select object_id
+  from sys.objects
+  where name = 'dimSales_2011')
+  AND (b.name IS NOT NULL)
+  AND (a.avg_fragmentation_in_percent != 0)
+ORDER BY a.avg_fragmentation_in_percent desc;
+
+ 
+-- add another column to show reorganize or rebuild based on criteria
+with c as 
+(select
+  a.index_id,
+  b.name as index_name,
+  a.avg_fragmentation_in_percent as degree
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) as a
+  INNER JOIN sys.indexes as b
+  ON a.index_id = b.index_id
+WHERE b.object_id = (
+  select object_id
+  from sys.objects
+  where name = 'dimSales_2011')
+  AND (b.name IS NOT NULL)
+  AND (a.avg_fragmentation_in_percent != 0)
+)
+select 
+      c.index_id, 
+      c.index_name, 
+      CASE 
+        WHEN (degree BETWEEN 5 AND 30) THEN 'reorganize'
+        WHEN (degree > 30) THEN 'rebuild'
+        ELSE 'no_action'
+      END AS to_do 
+from c
+
+-- remove ORDER BY a.avg_fragmentation_in_percent desc - it is not allowed in view ro subquery.
+
+-- WARNING
+-- above code gave misleading values
+
+-- let's do this way
+
+
+SELECT i.object_id, i.name, i.index_id
+FROM sys.indexes i
+WHERE object_id = (
+      select object_id
+      from sys.objects
+      where name = 'dimSales_2011'
+    );
+
+select 
+      a.database_id, 
+      a.object_id,
+      a.index_id,
+      a.partition_number,
+      a.avg_fragmentation_in_percent,
+      a.fragment_count
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) AS a
+
+-- return index name, fragmentation % and fragment count using above 2 tables
+
+select 
+      i.name,
+      a.avg_fragmentation_in_percent,
+      a.fragment_count
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) AS a
+INNER JOIN sys.indexes i
+ON (i.object_id = a.object_id )
+    AND (a.index_id = i.index_id)
+
+-- categorize them into reorganize and rebuild
+
+with desired_table as 
+(
+  select 
+      i.name as index_name,
+      a.avg_fragmentation_in_percent as degree,
+      a.fragment_count as fragment_count
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) AS a
+INNER JOIN sys.indexes i
+ON (i.object_id = a.object_id )
+    AND (a.index_id = i.index_id)
+)
+SELECT desired_table.index_name as index_name,
+      CASE 
+        WHEN (degree BETWEEN 5 AND 30) THEN 'reorganize'
+        WHEN (degree > 30) THEN 'rebuild'
+        ELSE 'no_action'
+      END AS to_do,
+      desired_table.degree as degree,
+      desired_table.fragment_count as fragment_count
+FROM desired_table
+ORDER BY degree desc;
+
+-- from above table I can specifically ask for dimSales_2011 if I know their index name
+
+-- actual_query
+with desired_table as 
+(
+  select 
+      i.name as index_name,
+      a.avg_fragmentation_in_percent as degree,
+      a.fragment_count as fragment_count
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  ) AS a
+INNER JOIN sys.indexes i
+ON (i.object_id = a.object_id )
+    AND (a.index_id = i.index_id)
+)
+SELECT desired_table.index_name as index_name,
+      CASE 
+        WHEN (degree BETWEEN 5 AND 30) THEN 'reorganize'
+        WHEN (degree > 30) THEN 'rebuild'
+        ELSE 'no_action'
+      END AS to_do,
+      desired_table.degree as degree,
+      desired_table.fragment_count as fragment_count
+FROM desired_table
+WHERE index_name LIKE 'Sales_%'
+ORDER BY degree desc;
+
+-- results show that only Sales_nonCL needs reorganize and no rebuild for any index
+
+
+-- stats before reorganizing for Sales-nonCL
+select *
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  )
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011') AND index_id = 2
+
+
+-- REORGANIZE
+
+USE AdventureWorksDW2022;
+GO
+
+ALTER INDEX [Sales_nonCL] 
+ON [dbo].[dimSales_2011]
+REORGANIZE;
+
+--  query_action
+select *
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  )
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011') AND index_id = 2
 
 
 
@@ -12,6 +258,55 @@
 
 **************************************************************/
 
+-- For the purpose of the demonstration, let us assume that the degree of fragmentation of Sales_NCCI non clustered columnstore index is greater than thirty percent.
+
+--  query_action index = 3
+select *
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  )
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011') AND index_id = 3
+
+-- index depth is 2
+-- page count is 5
+
+-- REBUILD Sales_NCCI
+
+USE AdventureWorksDW2022;
+GO
+
+ALTER INDEX [Sales_NCCI] 
+ON [dbo].[dimSales_2011]
+REBUILD PARTITION = ALL with
+( DATA_COMPRESSION = COLUMNSTORE);
+GO
+
+--  query_action index = 3
+select *
+from sys.dm_db_index_physical_stats(
+  DB_ID(N'AdventureWorksDW2022'),
+  OBJECT_ID(N''), 
+  NULL, 
+  NULL , 
+  NULL
+  )
+WHERE object_id = (select object_id
+from sys.objects
+where name = 'dimSales_2011') AND index_id = 3
 
 
+/*
+TO-DO:
+Error Handling and Validation: Consider adding error handling and validation checks, especially when executing DDL statements like ALTER INDEX. This can help prevent unintended consequences or errors in production environments.
 
+Documentation:  Ensure consistency and clarity in your comments throughout the script.
+
+Testing: It's essential to thoroughly test your queries, especially when performing index maintenance operations like reorganizing or rebuilding indexes. Consider testing in a non-production environment before applying changes in a production environment.
+
+*/
--- DB01_Developing_a_SQL_Databases/SQL_05_Maintaining_Columnstore_Indexes/Queries_results.md
+++ DB01_Developing_a_SQL_Databases/SQL_05_Maintaining_Columnstore_Indexes/Queries_results.md
@@ -2,11 +2,88 @@
 
   ## **Demonstrate how to reorganize a columnstore index**
 
+  ### INDEX FRAGMENTATION
 
+  -  After one or more data loads, the information in the index become scattered in a database. This is known as index fragmentation.
+  - This reduce query and then application performance.
+  - Solution to fix this: 
+    - Reorganize or Rebuild the respective index.
+  
+  Demonstration is given below:
+
+  - In [Queries.sql](DB01_Developing_a_SQL_Databases\SQL_04_Creating_Columnstore_Indexes\Queries.sql), we have created columnstore index for dimSales_2011.
+
+  - Use table-valued function: sys.dm_db_index_physical_stats
+    - returns information about the physical characteristics of indexes.
+    - useful for analyzing the fragmentation and storage of indexes in a database.
+    - arguments of function:
+      - Database_id (int)
+        - optional
+        - DB_ID(N'')
+      - object_id(int)
+        - optional
+        - OBJECT_ID(N'')
+      - index_id(int)
+        - optional
+        - NULL
+      - partition_id (int)
+        - optional
+        - NULL
+        - get number of partition.
+      - mode (int)
+        - Set 1: Get detailed stats.
+        - Set 0: Get basic stats.
+        - NULL
+    - N'': N: The uppercase letter "N" is a prefix that stands for National Language Character Set. When you precede a string literal with N, you're telling SQL Server that the string is a Unicode string. '': This is an empty string literal enclosed in single quotes. It represents an empty Unicode string.
+
+  What we are getting is based on this logic:
+  - get index id, index name and average fragmentation in percent
+  - Condition:
+    - filter to get info for object id of dimSales_2011.
+    - filter to remove null values of index name
+    - filter to remove where % is 0
+    - sort % in desc order
+
+    #### **if degree of fragmentation on an index is between 5% and 30%, reorganize the index.**
+
+    #### **if degree of fragmentation on an index is greater than 30%, rebuild the index.**
+
+![Index Fragmentation Stats for dimSales_2011](image.png)
+
+  - Now, create another column to categorize them as 
+    - reorganize
+    - rebuild
+
+![Index fragmentation: choose reorganize or rebuild](image-1.png)
+
+  - REORGANIZE Sales_NCCI with index id 2 that is non clustered columnstore index
+   
+ - New stats after actual_query
+
+![Fragmentation stats](image-2.png)
+
+  - REORGANIZE Sales_nonCL
+  - if you right click Sales_nonCL in smms -- it will give option to reorganize and rebuild as well
+
+  **What happens during REORGANIZING columnstore index**
+
+  - SQL server physically reorders the leaf-level pages and compacts the index pages.
+  - After running reorganize query and then query_action:
+    - index_depth reduced to 0 from 1.
+    - pages_count reduced to 0 as well.
+
+      Index Depth: The index depth refers to the number of levels in the index structure. A depth of 0 typically means that the index is now effectively a single-level index, meaning it's been heavily compacted and optimized. This reduction in depth suggests improved query performance as the index traversal becomes more efficient.
+
+      Page Count: The reduction in page count to 0 means that the index's physical storage has been compacted and optimized, possibly resulting in the removal of empty or unused pages. This optimization can lead to better storage efficiency and potentially faster data retrieval operations.
 
 
 # OBJECTIVE 02:
 
   ## **Demonstrate how to rebuild a columnstore index**
+
+
+  - REBUILD Sales_NCCI just to learn though it should not be based on degree of fragmentation
+  - if you right click Sales-NCCI in smms - it will give option to rebuild.
+  - Note: When you rebuild an index, the system drops an index and re-creates the index.
 
 
